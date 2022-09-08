827ec12bb345787799d52ae5cacf53e23725d19f
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-09-08 11:01:49 +0700

    [Learning Solidity] Events

--- events/Events.sol
+++ events/Events.sol
@@ -2,12 +2,19 @@
 pragma solidity ^0.8.13;
 
 contract Event {
+    // Event declaration
     event Log(string message, uint256 val);
-    // up to 3 index
+    // Up to 3 parameters can be indexed.
+    // Indexed parameters helps you filter the logs by the indexed parameter
     event IndexedLog(address indexed sender, uint256 val);
+    event Message(address indexed _from, address indexed _to, string message);
 
     function example() external {
         emit Log("foo", 123);
         emit IndexedLog(msg.sender, 789);
+    }
+
+    function sendMessage(address _to, string calldata message) external {
+        emit Message(msg.sender, _to, message);
     }
 }
---
+++ events/README.md
@@ -0,0 +1,6 @@
+# Events
+
+`Events` allow logging to the Ethereum blockchain. Some use cases for events are:
+
+- Listening for events and updating user interface
+- A cheap form of storage
