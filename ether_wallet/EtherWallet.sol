e0a4ba0410d860c32e39d9db512a8ef90bf7d089
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-09-19 15:30:12 +0700

    [Learning Solidity] Ether wallet

---
+++ ether_wallet/EtherWallet.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract EtherWallet {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == owner, "caller is not owner");
+
+        // use 'msg.sender' in memory is saving more gas than 'owner' (state variable)
+        payable(msg.sender).transfer(_amount);
+
+        /* bool(sent,) = payable(msg.sender).call{value: _amount}("");
+    require(sent, "Failed to withdraw"); */
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+}
